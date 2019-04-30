a94a3c91db6d58e178e304b7e5df9d1796fecd39
marek <pistekm@azet.sk>
2019-04-30 21:48:04 +0200

    add navigation classes

---
+++ TransportToStadiumSimulation/simulation/BusStopNavigationNode.cs
@@ -0,0 +1,18 @@
+﻿namespace TransportToStadiumSimulation.simulation
+{
+    public class BusStopNavigationNode
+    {
+        public BusStopNavigationNode Next { get; set; }
+        public double TimeToNext { get; }
+
+        public string Name { get; }
+        public int Id { get; }
+
+        public BusStopNavigationNode(double timeToNext, string name, int id)
+        {            
+            TimeToNext = timeToNext;
+            Name = name;
+            Id = id;
+        }
+    }
+}
---
+++ TransportToStadiumSimulation/simulation/Navigation.cs
@@ -0,0 +1,21 @@
+﻿namespace TransportToStadiumSimulation.simulation
+{
+    public class Navigation
+    {
+        public BusStopNavigationNode CurrentBusStopNavigationNode { get; private set; }
+
+        public Navigation(BusStopNavigationNode startBusStop)
+        {
+            CurrentBusStopNavigationNode = startBusStop;
+        }
+
+        public BusStopNavigationNode Next => CurrentBusStopNavigationNode.Next;
+
+        public double TimeToNext => CurrentBusStopNavigationNode.TimeToNext;
+
+        public void MoveToNext()
+        {
+            CurrentBusStopNavigationNode = CurrentBusStopNavigationNode.Next;
+        }
+    }
+}
