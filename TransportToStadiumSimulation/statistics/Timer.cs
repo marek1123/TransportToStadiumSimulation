180c5e6c91cc8e5e4265ba5440e4b1b97ba0a008
marek <pistekm@azet.sk>
2019-04-30 21:45:13 +0200

    add timer

---
+++ TransportToStadiumSimulation/statistics/Timer.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace TransportToStadiumSimulation.statistics
+{
+    public class Timer
+    {
+        double runningFromTime;
+        double runningTimeSum;
+        bool running = false;
+
+        public void Reset()
+        {
+            runningFromTime = 0;
+            runningTimeSum = 0;
+            running = false;
+        }
+
+        public void Start(double time)
+        {
+            if (running)
+            {
+                throw new InvalidOperationException("Timer is already running");
+            }
+            runningFromTime = time;
+            running = true;
+        }
+
+        public void Stop(double time)
+        {
+            if (!running)
+            {
+                throw new InvalidOperationException("Only running timer can be stopped.");
+            }
+            runningTimeSum += (time - runningFromTime);
+            running = false;
+        }
+
+        public double ActualTime(double time)
+        {
+            if (!running)
+            {
+                throw new InvalidOperationException("Timer is not running.");
+            }
+            return (time - runningFromTime);
+        }
+
+        public double Sum(double time)
+        {
+            if (running)
+            {
+                return runningTimeSum + ActualTime(time);
+            }
+            else
+            {
+                return runningTimeSum;
+            }
+        }
+
+        public double Sum()
+        {
+            return runningTimeSum;
+        }
+    }
+}
